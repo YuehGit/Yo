8ce69cb8b9d6d809f114f00f1283844d342e2c32
Yue <yue8757@gmail.com>
2016-08-23 15:23:22 -0400

    3. Add some simple animations.

--- app/src/main/java/com/yue/yo/MainActivity.java
+++ app/src/main/java/com/yue/yo/MainActivity.java
@@ -3,6 +3,8 @@ package com.yue.yo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -14,40 +16,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn1 = (Button) findViewById(R.id.btn_1);
+        final Animation animation = AnimationUtils.loadAnimation(this, R.anim.text_anim);
+
+        final Button btn1 = (Button) findViewById(R.id.btn_1);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                btn1.startAnimation(animation);
                 EditText editText = (EditText) findViewById(R.id.name);
                 String name = editText.getText().toString();
                 Toast.makeText(MainActivity.this, "Yo! " + name, Toast.LENGTH_LONG).show();
             }
         });
 
-        Button btn2 = (Button) findViewById(R.id.btn_2);
+        final Button btn2 = (Button) findViewById(R.id.btn_2);
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                btn2.startAnimation(animation);
                 EditText editText = (EditText) findViewById(R.id.name);
                 String name = editText.getText().toString();
                 Toast.makeText(MainActivity.this, "Yo!Yo! " + name, Toast.LENGTH_LONG).show();
             }
         });
 
-        Button btn3 = (Button) findViewById(R.id.btn_3);
+        final Button btn3 = (Button) findViewById(R.id.btn_3);
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                btn3.startAnimation(animation);
                 EditText editText = (EditText) findViewById(R.id.name);
                 String name = editText.getText().toString();
                 Toast.makeText(MainActivity.this, "Mo! " + name, Toast.LENGTH_LONG).show();
             }
         });
 
-        Button btn4 = (Button) findViewById(R.id.btn_4);
+        final Button btn4 = (Button) findViewById(R.id.btn_4);
         btn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                btn4.startAnimation(animation);
                 EditText editText = (EditText) findViewById(R.id.name);
                 String name = editText.getText().toString();
                 Toast.makeText(MainActivity.this, "Or! " + name, Toast.LENGTH_LONG).show();
