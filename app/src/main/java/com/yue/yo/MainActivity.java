f7e3292ed6067a56555312b2f0a32c7d26f207b0
Yue <yue8757@gmail.com>
2016-08-23 15:10:16 -0400

    2. Make the basic function using "toast".

--- app/src/main/java/com/yue/yo/MainActivity.java
+++ app/src/main/java/com/yue/yo/MainActivity.java
@@ -2,6 +2,10 @@ package com.yue.yo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button) findViewById(R.id.btn_1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText = (EditText) findViewById(R.id.name);
+                String name = editText.getText().toString();
+                Toast.makeText(MainActivity.this, "Yo! " + name, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button btn2 = (Button) findViewById(R.id.btn_2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText = (EditText) findViewById(R.id.name);
+                String name = editText.getText().toString();
+                Toast.makeText(MainActivity.this, "Yo!Yo! " + name, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button btn3 = (Button) findViewById(R.id.btn_3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText = (EditText) findViewById(R.id.name);
+                String name = editText.getText().toString();
+                Toast.makeText(MainActivity.this, "Mo! " + name, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button btn4 = (Button) findViewById(R.id.btn_4);
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText = (EditText) findViewById(R.id.name);
+                String name = editText.getText().toString();
+                Toast.makeText(MainActivity.this, "Or! " + name, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
